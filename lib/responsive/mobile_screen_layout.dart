1c46527712ed8e20091a0275467470b09e6d2218
Pavin Das <pavindas007@gmail.com>
2024-01-06 18:13:32 +0530

    App bar created

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,7 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:peeps/models/user.dart' as model;
-import 'package:peeps/providers/user_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:peeps/constants/colors.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
@@ -11,14 +10,95 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  int _page = 0;
+  late PageController pageController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    pageController.dispose();
+  }
+
+  navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  onPageChanged(int page) {
+    setState(() {
+      _page = page;
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    model.User user = Provider.of<UserProvider>(context).getUser;
+    // model.User user = Provider.of<UserProvider>(context).getUser;
     return Scaffold(
-      body: Center(
-        child: Text(
-          user.username,
-        ),
+      body: PageView(
+        children: [
+          Text('feed'),
+          Text('search'),
+          Text('add'),
+          Text('fav'),
+          Text('profile'),
+        ],
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        onPageChanged: onPageChanged,
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: secColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: _page == 0 ? whiteColor : bgColor,
+            ),
+            label: '',
+            backgroundColor: bgColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              color: _page == 1 ? whiteColor : bgColor,
+            ),
+            label: '',
+            backgroundColor: bgColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_circle,
+              color: _page == 2 ? whiteColor : bgColor,
+            ),
+            label: '',
+            backgroundColor: bgColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite,
+              color: _page == 3 ? whiteColor : bgColor,
+            ),
+            label: '',
+            backgroundColor: bgColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              color: _page == 4 ? whiteColor : bgColor,
+            ),
+            label: '',
+            backgroundColor: bgColor,
+          ),
+        ],
+        onTap: navigationTapped,
       ),
     );
   }
